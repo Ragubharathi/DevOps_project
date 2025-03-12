dbd9444dc5207b39e2a990f1292a5ea7578f002f
eksuser <ragupathi121290@gmail.com>
2025-03-12 14:30:12 +0530

    terraform.tfvars eleted

---
+++ firstproject/main.tf
@@ -0,0 +1,12 @@
+module "vpc" {
+  source = "./Modules/vpc"
+  my_region = "ap-south-1"
+  vpc_cidr = "100.100.0.1"
+  AZ1 = "ap-south-1a"
+  AZ2 = "ap-south-1b"
+  pub_sub = "100.100.1.1"
+  pri_sub = "100.100.2.1"
+  tags = "firstproject"
+  cluster_name = "eks_cluster_firstproject"
+  key_name     = "eks-keypair"
+}
