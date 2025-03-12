d8f9ab514e5a3c8cc01bf04834da7c9a8bdb2eb1
eksuser <ragupathi121290@gmail.com>
2025-03-12 17:51:22 +0530

    tested

--- firstproject/main.tf
+++ firstproject/main.tf
@@ -1,10 +1,10 @@
 module "vpc" {
   source = "/root/DevOps_project/Modules/vpc"
   my_region = "ap-south-1"
-  vpc_cidr = "100.100.0.0"
+  vpc_cidr = "10.0.0.0/16"
   AZ1 = "ap-south-1a"
   AZ2 = "ap-south-1b"
-  pub_sub = "100.100.1.1"
-  pri_sub = "100.100.2.1"
+  pub_sub = "10.0.1.0/24"
+  pri_sub = "10.0.2.0/24"
   tags = "firstproject"
 }
